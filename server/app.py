eced50fc263ba764ec8ed55460575078f8655da8
G-Zanc <gzancanelli3@gmail.com>
2023-03-07 13:11:58 -0500

    Python Service Changes

--- server/app.py
+++ server/app.py
@@ -1,15 +1,49 @@
 from flask import Flask, jsonify, request
+import datetime
 import openai
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from pathlib import Path
+import os
 
+path = Path('./config.env')
+load_dotenv(dotenv_path=path)
+ATLAS_URL = os.getenv('ATLAS_URI')
 app = Flask(__name__)
-openai.api_key = "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"
+client = MongoClient(ATLAS_URL)
+db = client.AteIt
+users = db.users
 
-@app.route('/gpt3', methods=['POST'])
+#openai.api_key = "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"
+
+@app.route('/message', methods=['POST'])
 def gpt3_response():
     data = request.get_json()
     print(data)
     prompt = data['prompt']
-    completions = openai.Completion.create(
+    user = data['email']
+    if prompt == "" or user == "": 
+        response = {
+            "response": "no information provided, please try again"
+        }
+        return jsonify(response)
+    
+    message = {
+        "text": prompt,
+        "date": datetime.datetime.now(),
+        "byUser": True
+    }
+
+
+    result = users.update_one({'email': user}, {'$push': {'messages': message}})
+    if result.modified_count == 0:
+        response = {
+            "response": "error occurred try again"
+        }
+        return(jsonify(response))
+
+
+    '''completions = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
         max_tokens=1024,
@@ -19,9 +53,18 @@ def gpt3_response():
     )
     response = {
         "response": completions.choices[0].text.strip()
+    }'''
+
+    response = {
+        "response": "prompt: {} Response: Testing Hello World!".format(prompt),
+        "date": datetime.datetime.now(),
+        "byUser": False
     }
 
+    users.update_one({'email': user}, {'$push': {'messages': response}})
+
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
